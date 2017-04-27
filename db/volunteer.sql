551ecd6956cad4ad41e99edc556f1a7f76feb694
NiharaDS <suhashi.desilva@gmail.com>
2017-04-27 05:43:29 +0530

    database altered

--- db/volunteer.sql
+++ db/volunteer.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2017 at 01:49 AM
+-- Generation Time: Apr 27, 2017 at 02:12 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -77,20 +77,6 @@ CREATE TABLE IF NOT EXISTS `others` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `personal_details`
---
-
-CREATE TABLE IF NOT EXISTS `personal_details` (
-  `school_id` int(50) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `occupation` varchar(100) NOT NULL,
-  `tel_no` int(10) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `school_details`
 --
 
@@ -100,8 +86,12 @@ CREATE TABLE IF NOT EXISTS `school_details` (
   `address` varchar(100) NOT NULL,
   `province` varchar(50) NOT NULL,
   `school_telno` int(10) NOT NULL,
+  `contact_person` varchar(50) NOT NULL,
+  `occupation` varchar(50) NOT NULL,
+  `contact_no` int(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
   PRIMARY KEY (`school_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
